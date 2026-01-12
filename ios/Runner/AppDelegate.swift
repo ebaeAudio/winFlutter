4ae63b2cd021a10492501ecf4a839ba05d447db6
ebaeAudio <evan.beyrer@audiochuck.com>
2026-01-11 19:14:32 -0500

    app delegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -22,7 +22,8 @@ import UIKit
   }
 }
 #else
-import UIKit@UIApplicationMain
+import UIKit
+@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -32,3 +33,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   }
 }
 #endif
+
